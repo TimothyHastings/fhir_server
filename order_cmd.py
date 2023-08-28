9918b8476293f931fd7d1ded397285dab33ac9af
Tim Hastings <timothy.e.hastings@gmail.com>
2023-08-28 16:39:16 +1000

    Add files via upload
    Minor bug in loader.py to stop crash if insert filename not found.
    New Patients0 to 5 fhir files using integer id for better sorting
    New Observation1 and 2 .fhir files
    Example order_cmd.py update
    database.txt sample database setup - note remove any old records under Collection>Patient and Collection>Observation

--- order_cmd.py
+++ order_cmd.py
@@ -1,3 +1,23 @@
+#
+# TODO Order collection command.
+#
+
 def order_collection(schema, command_line):
     # Test command
     print("Order Collection")
+
+    # order <collection> on <attribute>
+    collection_name = command_line[1]
+    print(collection_name)
+    collection = schema.get_collection(collection_name)
+
+    if collection is None:
+        print("Invalid Collection")
+        return
+
+    # Test only
+    order = "ACC"
+    collection.sort("id", order)
+
+
+
